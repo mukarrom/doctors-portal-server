645894ffa61d603df3bef1744a9d04e309fbe5f4
mukarrom <mukarrom.hosain.1@gmail.com>
2022-11-20 00:58:12 +0600

    available route added

--- index.js
+++ index.js
@@ -22,6 +22,19 @@ async function run() {
 		const serviceCollection = client
 			.db('doctors_portal')
 			.collection('services');
+		const bookingCollection = client
+			.db('doctors_portal')
+			.collection('bookings');
+
+		/**
+		 * API Naming Convention
+		 * app.get('/service') // available services
+		 * app.get('/booking') // get all booking in this collection
+		 * app.get('/booking/:id') // get a specific booking
+		 * app.post('/booking') // add a new booking
+		 * app.patch('/booking/:id') // update a booking
+		 * app.delete('/booking/:id') // delete a booking
+		 */
 
 		app.get('/services', async (req, res) => {
 			const query = {};
@@ -29,6 +42,77 @@ async function run() {
 			const services = await cursor.toArray();
 			res.send(services);
 		});
+
+		// Post : /booking
+		app.post('/booking', async (req, res) => {
+			const booking = req.body;
+			const query = {
+				treatment: booking.treatment,
+				date: booking.date,
+				patient: booking.patient,
+			};
+			const exists = await bookingCollection.findOne(query);
+			if (exists) {
+				return res.send({ success: false, booking: exists });
+			}
+			const result = await bookingCollection.insertOne(booking);
+			res.send({ success: true, booking: result });
+		});
+
+		// >-> get : available
+		// >-> do not use this in the future. rather use aggregate, learn more about mongodb
+		app.get('/available', async (req, res) => {
+			const date = req.query.date;
+			// step 1: get all service
+			const services = await serviceCollection.find().toArray();
+
+			const query = { date: date };
+
+			// step 2: get the booking of that day
+			const bookings = await bookingCollection.find(query).toArray();
+
+			// step 3: for each service
+			services.forEach(service => {
+				// step 4: find booking for this service. out: [{},{},{}]
+				const serviceBookings = bookings.filter(
+					book => book.treatment === service.name
+				);
+
+				// step 5: select slots for the service bookings: ["","",""]
+				const bookedSlot = serviceBookings.map(booked => booked.slot);
+
+				// step 6: select those slots that are not in bookedSlot
+				const available = service.slots.filter(
+					slot => !bookedSlot.includes(slot)
+				);
+				// step 7: set available to slots to make it easier
+				service.slots = available;
+			});
+
+			res.send(services);
+		});
+
+		// app.get('/available', async (req, res) => {
+		// 	const date = req.query.date || 'Nov 19, 2022';
+
+		// 	// step 1: get all services
+		// 	const services = await serviceCollection.find().toArray();
+		// 	// step 2: get the booking of that day
+		// 	const query = { date: date };
+		// 	const bookings = await bookingCollection.find(query).toArray();
+
+		// 	// step 3: for each service, find booking for that service
+		// 	services.forEach(service => {
+		// 		const serviceBookings = bookings.filter(
+		// 			b => b.treatment === service.name
+		// 		);
+		// 		const booked = serviceBookings.map(s => s.slot);
+		// 		const available = service.slots.filter(s => !booked.includes(s));
+		// 		service.available = available;
+		// 	});
+
+		// 	res.send(services);
+		// });
 	} finally {
 	}
 }
